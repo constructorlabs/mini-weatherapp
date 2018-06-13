4af17dc23304a57c043ccd3559770b5b157bb4d9
Ollie Crook <olliejcrook@gmail.com>
2018-06-13 11:13:40 +0100

    add image from thumbs to figure

--- assets/src/index.js
+++ assets/src/index.js
@@ -10,16 +10,17 @@ function submitHandler(event){
   }).then(function(jsonData){
     const weatherDescription = jsonData.weather[0].description;
 
-    fetch(`https://api.unsplash.com/search/photos?query=${weatherDescription}&client_id=f3dc3b6bb3462a90c7969a62d83c8e3dd552b928cc31041850388e8616e977a6`)
-    .then(function(response){
-      return response.json();
-    }).then(function(data){
-      const firstImage = `<img src="${data.results[0].links.download}"></img>`;
-      figure.innerHTML = firstImage;
-      console.log(data);
-    }).catch(function(error){
-      alert('I don\'t work');
-    });
+    return fetch(`https://api.unsplash.com/search/photos?query=${weatherDescription}&client_id=f3dc3b6bb3462a90c7969a62d83c8e3dd552b928cc31041850388e8616e977a6`)
+
+
+  }).then(function(response){
+    return response.json();
+  }).then(function(data){
+    images = data.results.map(image =>
+      `<img src="${image.links.download}" id="${image.id}" class="thumbs__link"></img>`
+    )
+    thumbs.innerHTML = images;
+    console.log(data);
 
   }).catch(function(error){
     alert('I don\'t work');
@@ -30,4 +31,13 @@ const getSearch = document.querySelector("#search");
 
 const figure = document.querySelector('#photo');
 
+const thumbs = document.querySelector('#thumbs');
+
+let images;
 getSearch.addEventListener("submit", submitHandler);
+
+function clickHandler(event){
+  figure.innerHTML= `<img src="${event.target.src}" class="photo"></img>`;
+}
+
+thumbs.addEventListener("click", clickHandler);
