05862c93b1a18a412497a99a0f9a80ba2b848733
Alex <alexandrubaltag75@yahoo.com>
2018-06-13 17:04:28 +0100

    updated for any city

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 //const imagesReqKey='https://api.unsplash.com/search/photos?query=snow&client_id=217d39354a152114b5bb4e0f77e4ca3c7ecdbe155bb9d43580e11138def7915a';
 
 
-const goButton = document.querySelector('.btn');
+//const goButton = document.querySelector('.btn');
 const search = document.querySelector('#search');
 const foreCast = document.querySelector('#conditions');
 const smallPictures = document.querySelector('.thumbs');
@@ -10,7 +10,7 @@ const largeImage = document.querySelector('.photo');
 
 //////get weather info//////////////////////////////////
 function getWeather(city){
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=london&APPID=b7aecc81c3f01e1eadcb367a666606c4`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=b7aecc81c3f01e1eadcb367a666606c4`)
     .then(function(response){
       return response.json();
     })
@@ -52,16 +52,17 @@ function getWeather(city){
 ////event listener/////////////////////////////////
 search.addEventListener('submit', function(event) {
   event.preventDefault();
-        getWeather();
+      //console.log(event.target.city.value);
+        getWeather(event.target.city.value);
 });
 
-
+//////event get the picture from thumb and add to photo section /////////
 smallPictures.addEventListener('click',function(event){
-if(event.target.className !== 'thumbs__link')return
-const test=event.target.getAttribute('href');
-event.preventDefault();
-//console.log(event.target.className);
-
-largeImage.innerHTML=`<img src=${test}/>`
+  if(event.target.className !== 'thumbs__link')  //prevent to click on different area 
+return
+  const test=event.target.getAttribute('href');
+  event.preventDefault();
+  //console.log(event.target.className);
+  largeImage.innerHTML=`<img src=${test}/>`
 
 })
