58c804ca8f39a3a3b6ace14de8f050da8932a4f3
Ralph <ralph@rgalan.es>
2018-06-13 14:18:38 +0100

    Added thumbs list

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,10 @@ const weatherAPIKey = "4afe87cd7eddc53b1473c2bc0ddb2e37";
 const unsplashAPIKey =
   "17de354439973eb34b4aae66cb3c27ceea000efc05a8c6c94da17edac9cd3a7b";
 const form = document.querySelector("#search");
+const mainImage = document.querySelector("#photo");
+const thumbnails = document.querySelector("#thumbs");
+const input = form.querySelector("#search-tf");
+const conditions = document.querySelector("#conditions");
 
 function createRequest(service, request) {
   return service === "weather"
@@ -10,11 +14,35 @@ function createRequest(service, request) {
 }
 
 // api.openweathermap.org/data/2.5/weather?q=$london&APPID=4afe87cd7eddc53b1473c2bc0ddb2e37
+function displayImage(imageData, size, numToShow) {
+  const imageArr = imageData.results;
+  let html = "";
+  if (numToShow) {
+    for (let i = 0; i < numToShow; i += 1) {
+      html += `
+         <img src="${imageArr[i].urls[size]}" />
+         `;
+    }
+  } else {
+    let markup = imageArr
+      .map((item, index) => {
+        return `
+         <li>
+            <a id="${item.id}" class="thumbs__link" alt="${
+          item.description ? item.description : conditions.textContent
+        }">
+                <img src="${item.urls[size]}" />
+            </a>
+        </li>
+         `;
+      })
+      .join("");
+    html = `<ul class="thumbs__list">${markup}</ul>`;
+  }
+  return html;
+}
 
-http: function submitForm(e) {
-  const input = form.querySelector("#search-tf");
-  const conditions = document.querySelector("#conditions");
-
+function submitForm(e) {
   e.preventDefault();
   fetch(createRequest("weather", input.value))
     .then(function(weatherResponse) {
@@ -28,7 +56,8 @@ http: function submitForm(e) {
       return imageResponse.json();
     })
     .then(function(imageData) {
-      console.log(imageData);
+      mainImage.innerHTML = displayImage(imageData, "full", 1);
+      thumbnails.innerHTML = displayImage(imageData, "thumb");
     })
     .catch(function(error) {
       console.log(error);
