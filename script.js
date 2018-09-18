b255f4f91a9a3a12476ec3583b8fa5a3404cd555
Tony Batty <tonyeb87@gmail.com>
2018-09-18 11:36:27 +0100

    displaying thumbnails

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 console.log("hello World");
 
 const generateApi = () => {
-    fetchContent(openWeatherApi);
+    fetchWeather(openWeatherApi);
     
 };
 
 
-function displayContent(body) {
+function getDescription(body) {
     console.log(body.weather[0].description);
     const weatherDescription = body.weather[0].description;
     const weatherURL = `https://api.unsplash.com/search/photos?page=1&query=${weatherDescription}&client_id=d1463f432cce4150640ff56ee13c1f94ec0b2993db4395bcb8913f34daeb0d48`
+    
+    fetchPhotos(weatherURL);
+    // console.log(body);
+}
+
+function displayPhotos(body) {
+    console.log(body);
+    const images = body.results;
+    const thumbParent = document.querySelector(".thumbs");
+    images.forEach((image, index) => {
+        console.log(image.urls.full);
+        thumbParent.innerHTML += `<img class="thumb" id="${index}" src="${image.urls.thumb}">`
+    })
 }
 
 
@@ -19,7 +32,7 @@ const unsplashApi = 'https://api.unsplash.com/search/photos?page=1&query=office&
 
 
 
-function fetchContent(url){
+function fetchWeather(url){
     // main news body fetch - button changeable
     fetch(url) // by default fetch makes a GET request
     .then(function(response) {
@@ -28,7 +41,20 @@ function fetchContent(url){
     })
     .then(function(body){
       //  parentNode.innerHTML = "";
-        displayContent(body);
+        getDescription(body);
+    });   
+}
+
+function fetchPhotos(url){
+    // main news body fetch - button changeable
+    fetch(url) // by default fetch makes a GET request
+    .then(function(response) {
+        
+        return response.json();
+    })
+    .then(function(body){
+      //  parentNode.innerHTML = "";
+        displayPhotos(body);
     });   
 }
 
